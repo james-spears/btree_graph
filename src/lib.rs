134c5478e6bfc7668596b8bf952ceec857ed9700
James Spears <james.w.spears@gmail.com>
2021-01-19 17:14:48 -0500

    add top level mod comments, change defaults for docs purposes

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 #![no_std]
 extern crate alloc;
 
+/// `encoding` module contains the definition of the encoding API and auto implementations.
 #[cfg(any(feature = "serde_cbor", feature = "serde_json", feature = "serde_yaml"))]
 pub mod encoding;
+
+/// `error` module contains the definition of the Error struct.
 pub mod error;
 
 mod graph;
